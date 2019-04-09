68d15d0b19e67350d09eabbb7bfe09566559a67c
Raffaele Aldrigo <reloader90@gmail.com>
2019-04-09 18:50:49 +0200

    filter emotion

--- ReplyVision/MainWindow.xaml.cs
+++ ReplyVision/MainWindow.xaml.cs
@@ -119,33 +119,38 @@ namespace ReplyVision
                 .Append("Emotion: ");
 
             Emotion emotionScores = face.FaceAttributes.Emotion;
-            if (emotionScores.Anger >= 0.1f) sb.Append(
-                String.Format("anger {0:F1}%, ", emotionScores.Anger * 100));
-            if (emotionScores.Contempt >= 0.1f) sb.Append(
-                String.Format("contempt {0:F1}%, ", emotionScores.Contempt * 100));
-            if (emotionScores.Disgust >= 0.1f) sb.Append(
-                String.Format("disgust {0:F1}%, ", emotionScores.Disgust * 100));
-            if (emotionScores.Fear >= 0.1f) sb.Append(
-                String.Format("fear {0:F1}%, ", emotionScores.Fear * 100));
-            if (emotionScores.Happiness >= 0.1f) sb.Append(
-                String.Format("happiness {0:F1}%, ", emotionScores.Happiness * 100));
-            if (emotionScores.Neutral >= 0.1f) sb.Append(
-                String.Format("neutral {0:F1}%, ", emotionScores.Neutral * 100));
-            if (emotionScores.Sadness >= 0.1f) sb.Append(
-                String.Format("sadness {0:F1}%, ", emotionScores.Sadness * 100));
-            if (emotionScores.Surprise >= 0.1f) sb.Append(
-                String.Format("surprise {0:F1}%, ", emotionScores.Surprise * 100));
+
+            var dict= new Dictionary<string, double>();
+            dict.Add("Anger", emotionScores.Anger );
+            dict.Add("Comptempt", emotionScores.Contempt);
+            dict.Add("Disgust", emotionScores.Disgust);
+            dict.Add("Fear", emotionScores.Fear);
+            dict.Add("Happy", emotionScores.Happiness);
+            dict.Add("Neutral", emotionScores.Neutral);
+            dict.Add("Sad", emotionScores.Sadness);
+            dict.Add("Surprise", emotionScores.Surprise);
+
+            var max = dict.First();
+            foreach( var kv in dict ) {
+                if( kv.Value > max.Value )
+                    max = kv;
+            }
+
+            sb.Append( max.Key ).Append( ", " );
 
             // Add glasses.
-            sb.Append(face.FaceAttributes.Glasses);
-            sb.Append(", ");
+            sb.Append(face.FaceAttributes.Glasses).Append(", ");
 
             // Add hair.
             sb.Append("Hair: ");
 
             // Display baldness confidence if over 1%.
-            if (face.FaceAttributes.Hair.Bald >= 0.01f)
-                sb.Append(String.Format("bald {0:F1}% ", face.FaceAttributes.Hair.Bald * 100));
+            if( face.FaceAttributes.Hair.Bald > 0.80f )
+                sb.Append( "Calvo " );
+            else if( face.FaceAttributes.Hair.Bald >= 0.20f )
+                sb.Append( "Stempiato " );
+            else
+                sb.Append( "Coi capelli " );
 
             // Display all hair color attributes over 10%.
             IList<HairColor> hairColors = face.FaceAttributes.Hair.HairColor;
